2fdf9f26a7f22d105eae040fc5ce9a4fb7835870
ABHIRAMRACHAKONDA <68802081+ABHIRAMRACHAKONDA@users.noreply.github.com>
2022-05-24 20:13:47 +0530

    Add files via upload

---
+++ HealthDriver.java
@@ -0,0 +1,16 @@
+package com.greatlearning.corejava;
+
+public class HealthDriver {
+
+	public static void main(String[] args) {
+		Health h1 = new Health();
+		h1.age = 22;
+		h1.bmi = 5.9f;
+		h1.weight = 72;
+		
+System.out.println(h1.age);
+System.out.println(h1.bmi);
+System.out.println(h1.weight);
+	}
+
+}
