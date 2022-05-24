1c326aaf694b7052079eb67153e2b44f51e83ea9
ABHIRAMRACHAKONDA <68802081+ABHIRAMRACHAKONDA@users.noreply.github.com>
2022-05-24 20:20:33 +0530

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,7 +1,11 @@
+package com.greatlearning.corejava;
 
 public class Customer {
+	public static void main(String [] args) {
+		CustomerDriver c1 = new CustomerDriver();
+		System.out.println(c1.name);
+		System.out.println(c1.height);
+		System.out.println(c1.getAge());
+	}
 
-	public	String fullName;
-	public	int age;
-	
 }
---
+++ CustomerDriver.java
@@ -0,0 +1,14 @@
+package com.greatlearning.corejava;
+
+public class CustomerDriver {
+  public String name = "abhi";
+private int age = 24;
+protected float height = 5.9f;
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+}
